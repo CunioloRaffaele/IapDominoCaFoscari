b5833a654c8dac04c3c9521e828b8c4ab0c921ec
GitBariAl <91183150+GitBariAl@users.noreply.github.com>
2023-11-16 22:50:16 +0100

    for should be ok, but there are type errors that should be fixed

--- functions/challangeHandler.c
+++ functions/challangeHandler.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+//Questa funzione, preso l'array bidimensionale indice - indice card, cerca la card corrispondente nel deck e le stampa nel corretto ordine
+void printCards(int** deck, int** orderedDeck, int orderedDeckDimension) {
+    //Loop attraversio orderDeck
+    int lastIndex = -1;
+    
+    for (int i = 0; i < orderedDeckDimension; i++) {
+        if(lastIndex==-1){
+            printf("S");
+            printf("%d %d", deck[orderedDeckDimension[i][1]][0], deck[orderedDeckDimension[i][1]][1]);
+            lastIndex = orderedDeckDimension[i][1];
+        }else if(i<lastIndex){
+            printf("R");
+            printf("%d %d",deck[orderedDeckDimension[i][1]][0], deck[orderedDeckDimension[i][1]][1]);
+            lastIndex = orderedDeckDimension[i][1];
+        }else
+        {
+            printf("L");
+            printf("%d %d", deck[orderedDeckDimension[i][1]][0], deck[orderedDeckDimension[i][1]][1]);
+            lastIndex = orderedDeckDimension[i][1];
+        }
+        
+    }
+
+}
