7a2ec4942bb37c8b3a20de4d0a9192905e7c6e41
Cuniolo Raffaele <raffaelecuniolo@gmail.com>
2024-01-25 22:49:33 +0100

    documentazione doxygen per fragments.h

--- fragments.h
+++ fragments.h
@@ -18,12 +18,34 @@ void fragment1(int[21][2], int, int, bool);
 */
 void fragment2(bool, int[21][2]);
 
-// fragment3 (array, indice tessera iniziale, indice tessera selezionata, animazione da applicare a spawnScreenWithTitle, dimensione dell'array)
+/**
+* Questa schermata permette all'utente di visualizzare le tessere generate.
+* @param array gameset
+* @param int indice tessera iniziale
+* @param int indice tessera selezionata
+* @param bool animazione da applicare a spawnScreenWithTitle
+* @param int dimensione dell'array
+*/
 void fragment3(int**, int, int, bool, int);
 
+/**
+ * Questa schermata permette all'utente di giocare a Domino.
+ * @param array gameset
+ * @param int indice tessera iniziale
+ * @param int indice tessera selezionata
+ * @param bool animazione da applicare a spawnScreenWithTitle
+ * @param int dimensione dell'array
+ * @param gameNodeLinList lista di nodi contenente le tessere giocate
+ */
 typedef struct gameNodeLin gameNodeLinList;
 void fragment4(int**, int, int, bool, int, gameNodeLinList *);
 
+/**
+ * Questa schermata permette all'utente di giocare a 2D Domino.
+ * ATTENZIONE questa schermata modifica il deck del giocatore, quindi non è possibile tornare indietro.
+ * @param array gameset
+ * @param int dimensione dell'array
+ */
 typedef struct gameNode2DLin gameNode2DLinList;
 void fragment5(int**, int);
 
